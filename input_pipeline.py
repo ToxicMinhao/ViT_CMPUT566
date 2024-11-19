7cda3323adc8a575e34d942c58f9b8f57d311a51
W4LT3RM <jianming@ualberta.ca>
2024-11-18 21:58:03 -0700

    Adds random transformations to images

--- input_pipeline.py
+++ input_pipeline.py
@@ -19,3 +19,26 @@ def preprocess_image(image, label, image_size):
     image = (image - 0.5) * 2.0  # Scale to [-1, 1]
 
     return image, tf.one_hot(label, depth=1000)  # Assuming 1000 classes (ImageNet)
+
+def augment_image(image, label, image_size):
+    """
+    Augments the image with random transformations for training.
+    
+    Args:
+        image: Input image.
+        label: Corresponding label.
+        image_size: Target image size (height, width).
+        
+    Returns:
+        A tuple of augmented image and label.
+    """
+    # Random crop and resize
+    image = tf.image.random_crop(image, [image_size, image_size, 3])
+    image = tf.image.resize(image, [image_size, image_size])
+    image = tf.image.random_flip_left_right(image)
+
+    # Normalize and scale
+    image = tf.cast(image, tf.float32) / 255.0
+    image = (image - 0.5) * 2.0  # Scale to [-1, 1]
+
+    return image, label
